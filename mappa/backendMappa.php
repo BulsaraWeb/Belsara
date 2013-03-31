396b08c79734756308fa85f1d76524b1df73a4b6
Claudio Calabrese <latem_t@hotmail.com>
2013-03-31 04:02:40 +0200

    Modifica cat .5

--- mappa/backendMappa.php
+++ mappa/backendMappa.php
@@ -17,6 +17,9 @@ if($_SESSION['user']!='chief')
 		<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
 		<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=places&sensor=false"></script>
 		<script>
+		
+		var actualCatSelected=-1;
+		
 		function enableDisableButtonNewCat()
 		          {
 		          	
@@ -30,13 +33,24 @@ if($_SESSION['user']!='chief')
 		
 		function enableDisableButtonDelCat()
 		          {
-		          	
-	                 if($('#listaCategorie').val()<=0)
-	                   {
-	                    $('input[value=Elimina]').attr("disabled", "disabled");;
-	                    return;
-	                   } 
-	                   $('input[value=Elimina]').removeAttr("disabled");	
+		          	if(actualCatSelected!=-1)
+		          	 {
+		          	  $("#pin"+actualCatSelected).css("display","none");
+		          	  $('input[name=editDescr]').val("");
+		          	  $('input[name=editName]').val("");
+		          	 }
+	                if($('#listaCategorie').val()<=0)
+	                  {
+	                   $('input[value=Elimina]').attr("disabled", "disabled");;
+	                   return;
+	                   actualCatSelected=-1;
+	                  } 
+	                actualCatSelected=$('#listaCategorie').val();
+	                $('input[value=Elimina]').removeAttr("disabled");
+	                //alert(actualCatSelected);
+	                $("#pin"+actualCatSelected).css("display","block");
+	                $('input[name=editDescr]').val($("#cat"+actualCatSelected).attr("title"));
+	                $('input[name=editName]').val($("#listaCategorie option[value='"+actualCatSelected+"']").text());
 		          };
 		          
 		function enableDisableButtonNewLoc()
@@ -109,67 +123,77 @@ if($_SESSION['user']!='chief')
 		          
 		$(document).ready(function() 
 				{
-				
+				 actualCatSelected=-1;
 				});
 		</script>
 		</head>
 	<body>
 	 <?php
 	   require("markerModel.php");
-	  if(isset($_POST["catName"]) && isset($_POST["pin"]) && isset($_POST["description"]))
+	  if(isset($_POST["catName"]) && $_FILES["file"]["error"]==0 && isset($_POST["description"]))
  	    {
- 	    //	unlink($_SERVER["DOCUMENT_ROOT"]."/newSite/gallery".$i.".jpg");
- 	    echo $_FILES['pin'].'<br>';
-		 if(move_uploaded_file($_FILES['pin'],$_SERVER["DOCUMENT_ROOT"]."/newSite/mappa/pin-images/pin.jpg"))
- 	        echo "arrivo --> ";
+ 	     if((strstr($_FILES['pin']['name'],".png")))
+		   {
+		    move_uploaded_file($_FILES["pin"]["tmp_name"],$_SERVER["DOCUMENT_ROOT"]."newSite/mappa/pin-images/".$_POST["catName"].".png");
+   		    addCategory($_POST["catName"],"http://www.bulsara.it/newSite/mappa/pin-images/".$_POST["catName"].".png",$_POST["description"]);
+		    echo 'Inserimento avvenuto';
+		   }
 		 else
-		 	echo "ciccia --> ";
-   		 addCategory($_POST["catName"],$_SERVER["DOCUMENT_ROOT"]."/newSite/mappa/pin-images/pin.jpg",$_POST["description"]);
-		 echo 'Inserito  '.$_POST["catName"].'  '.$_SERVER["DOCUMENT_ROOT"]."/newSite/mappa/pin-images/pin.jpg".'  '.$_POST["description"];
-  		}
-  	  
+		   {
+		   	echo "L'immagine caricata non e' png";
+		   }
+  	  	}
 	  else if(isset($_POST['categoryList']))
 	    {
 	     
+ 	     //unlink($_SERVER["DOCUMENT_ROOT"]."newSite/mappa/pin-images/".     ?????     .".png");
 		 removeCategory($_POST['categoryList']);
+		 echo 'Categoria eliminata';
 	    }
 	   
-	   //removeCategoryFromName
 	   
-	   echo '<center><table width=80%>';
-	   echo '<tr>';
+	   
 	   
 	   // Category List
-	   echo '<td>';
 	   $cat=getCategories();
+	   echo '<center>';
 	   echo '<form action="backendMappa.php" method="post">';
-	   echo 'Categorie Presenti:<br><select id="listaCategorie" name="categoryList" id="category" size="8" onchange="enableDisableButtonDelCat();">';
+	   echo '<table><tr><td>';
+	   echo 'Categorie <br>Presenti:<br><select id="listaCategorie" name="categoryList" id="category" size="8" onchange="enableDisableButtonDelCat();">';
 	   foreach($cat as $k=>$v)
-              echo '<option value="'.$k.'">'.$v['catName'].'</option>';
-       echo '</select><br><input type="submit" value="Elimina" disabled>';
+              echo '<option id="cat'.$k.'" value="'.$k.'" title="'.$v['description'].'">'.$v['catName'].'</option>';
+       echo '</select></td><td>';
+	   echo 'Nome: <br><input id="editName" type="text" name="editName" onkeydown="">';
+	   echo '<br>Pin: ';
+       foreach($cat as $k=>$v)
+	         echo '<img id="pin'.$k.'" src="'.$v['pinPath'].'" style="display: none;">';
+	   echo '<br><input id="editPin" value="clicca per modificare" name="editPin" type="file" onkeydown="">';
+	   echo '<br><br>Descrizione: <br><input id="editDescr" type="text" name="editDescr" onkeydown="">';
+	   echo '</td></tr></table>';
+       echo '<br><br><input type="submit" value="Elimina" disabled>';
+	   echo '    <input type="submit" value="Modifica" disabled>';
 	   echo '</form>';
-	   echo '</td>';
+	   
+	   echo '<br><br><br><br><br><br>';
 	   
 	   // Insert new category
-	   echo '<td>';
-	   echo '<form action="backendMappa.php" method="post">';
+	   echo '<form action="backendMappa.php" method="post" enctype="multipart/form-data">';
 	   echo '<br>Nuova Categoria: <input id="catName" type="text" name="catName" onkeydown="enableDisableButtonNewCat();">';
 	   echo '<br>Immagine Pin: <input id="pin" name="pin" type="file" onkeydown="enableDisableButtonNewCat();">';
 	   echo '<br>Descrizione: <input id="description" type="text" name="description" onkeydown="enableDisableButtonNewCat();">';
 	   echo '<br><input type="submit" value="Inserisci" disabled >';
 	   echo '</form>';
-	   echo '</td>';
 	   
+	   echo '<br><br><br><br><br><br>';
 	   // Insert new location
-	   echo '<td>';
+	   
 	   echo '<form action="backendMappa.php" method="post">';
 	   echo '<br>Nuovo Partner (indirizzo): <input id="searchLoc" type="text" name="locName" onkeydown="enableDisableButtonNewLoc();">';
 	   echo '<input type="submit" value="Inserisci" id="insertLoc" disabled >';
 	   echo '</form>';
-	   echo '</td>';
-	   echo '</tr>';
+	   
 
-	   echo '</table></center>';
+	   echo '</center>';
        
 	  ?>
 
